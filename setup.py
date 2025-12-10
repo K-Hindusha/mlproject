42577dde08f1fab08219a4a4fd45cb3755731c7a
K-Hindusha <220701092@rajalakshmi.edu.in>
2025-12-10 19:00:27 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages,setup
+from typing import List
+HYPEN_E_DOT='-e .'
+
+def get_requirements(filepath)->List[str]:
+    requirements=[]
+    with open(filepath) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+
+setup(
+    name='MLProject',
+    author='Hindusha',
+    author_email='hindusha27@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
